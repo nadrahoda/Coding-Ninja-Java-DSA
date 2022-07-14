a8c67154f4a0fae56c5c20443dc02cabed0f6013
Nadra Hoda <88574218+nadrahoda@users.noreply.github.com>
2022-07-14 20:59:59 +0530

    Add files via upload

---
+++ Conditionals and Loops/SumOfEven.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class SumOfEven {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        
+        
+        int i = 1;
+        int sum = 0;
+        
+        while(i<=N){
+            if(i%2==0){
+                
+                sum = sum + i;
+                
+                
+            }
+            i++;
+           
+        }
+        System.out.println(sum);
+    }
+}
