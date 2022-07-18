9f9c56fe7fb953aaefded4716e2d6590d5ceb2f3
Nadra Hoda <88574218+nadrahoda@users.noreply.github.com>
2022-07-18 22:24:46 +0530

    Update Square_Pattern.java

--- Pattern 1/Square_Pattern.java
+++ Pattern 1/Square_Pattern.java
@@ -1,3 +1,16 @@
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+4444
+4444
+4444
+4444
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+
+
+
 /**
  * Square_Pattern
  */
